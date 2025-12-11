388c0fa185a9696378c0aad9fdd1da9c30393dc7
rishabhraaj <rishabhraj1976@gmail.com>
2025-12-11 19:14:59 +0530

    initial_commit

---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,47 @@
+package com.example.demo.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class Product {
+    @Id
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    private String category;
+
+    //constructor
+    public Product() {
+    }
+    public Product(Long id, String name, Double price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+    
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+    public String getCategory() {
+        return category;
+    }
+}
---
+++ src/main/java/com/example/demo/service/impl/ProductServiceImpl.java
@@ -0,0 +1,48 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
+import com.example.demo.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Product updateProduct(Long id, Product newProduct) {
+        return productRepository.findById(id)
+                .map(p -> {
+                    p.setName(newProduct.getName());
+                    p.setPrice(newProduct.getPrice());
+                    return productRepository.save(p);
+                }).orElse(null);
+    }
+
+    @Override
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
